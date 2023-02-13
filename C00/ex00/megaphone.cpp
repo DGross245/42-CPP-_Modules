e3a83a67ad1c5ef7e3dd9cd16d349256b43ea4e6
DGross245 <103501922+DGross245@users.noreply.github.com>
2023-02-13 19:26:23 +0100

    finished C00/ex00

--- C00/ex00/megaphone.cpp
+++ C00/ex00/megaphone.cpp
@@ -3,16 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
+/*   By: dna <dna@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 15:34:03 by dgross            #+#    #+#             */
-/*   Updated: 2023/02/11 16:27:28 by dgross           ###   ########.fr       */
+/*   Updated: 2023/02/13 19:04:52 by dna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
+#include <string>
 
 int main(int argc, char **argv)
 {
-	
+	int		i;
+	int 	j;
+
+	i = 0;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (argv[++i] != NULL)
+		{
+			for(j = 0; argv[i][j] != '\0'; ++j)
+			{
+				argv[i][j] = toupper(argv[i][j]);
+			}
+			std::cout << argv[i];
+		}
+		std::cout << std::endl;
+	}
+	return (0);
 }
