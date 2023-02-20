b61f0fffead875d02ad9c83a9ff2647397a22280
Daniel Gross <dgross@2-H-7.42heilbronn.de>
2023-02-20 17:40:27 +0100

    finished ex03

---
+++ C01/ex04/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgross <dgross@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 17:15:49 by dgross            #+#    #+#             */
+/*   Updated: 2023/02/20 17:37:40 by dgross           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+void replace (char *filename, char *s1, char *s2) {
+	std::fstream inputFile(filename);
+	std::string	line;
+
+	if (inputFile.is_open())
+	{
+		while(getline( inputFile, line))
+		{
+			
+		}
+		inputFile.close();
+	}
+	else
+		std::cout << "file doesnt exist" << std::endl;
+	return ;
+}
+
+int main( int argc, char **argv )
+{
+	if (argc != 4)
+	{
+		std::cout << "Wrong number of arg" << std::endl;
+		return (1);
+	}
+	replace(argv[1], argv[2], argv[3]);
+	return (0);
+}
